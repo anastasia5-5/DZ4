9e5687d34fb0c3db40a027cfef2aac635965c733
anastasia5-5 <novikovanastusha0509@gmail.com>
2024-11-21 00:34:39 +0300

    Добавьте файлы проекта.

---
+++ NovikovaNastya4/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+
+namespace NovikovaNastya4
+{
+    internal class Program
+    {
+        public void CalculateArrayStatistics(params int[] numbers)
+        {
+           
+            int sum = 0;
+            int product = 1;
+            double average;
+
+            
+            foreach (int number in numbers)
+            {
+                sum += number;
+                product *= number;
+            }
+
+            
+            average = (numbers.Length > 0) ? (double)sum / numbers.Length : 0;
+
+           
+            Console.WriteLine($"Сумма: {sum}");
+            Console.WriteLine($"Произведение: {product}");
+            Console.WriteLine($"Среднее арифметическое: {average}");
+        }
+
+        static void Main(string[] args)
+        {
+        Console.WriteLine("Задание 2");
+        Program program = new Program();
+
+        program.CalculateArrayStatistics(11, 4, 7, 9, 1);
+        Console.ReadLine();
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ NovikovaNastya_lab4/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+
+
+namespace NovikovaNastya_lab4
+{
+    internal class Program
+    {
+        static int GCD(int a, int b)
+        {
+            if (a <= 0 || b <= 0)
+            {
+                throw new ArgumentOutOfRangeException("Числа должны быть натуральными (больше нуля).");
+            }
+
+            while (b != 0)
+            {
+                int number = b;
+                b = a % b;
+                a = number;
+            }
+
+            return a;
+        }
+
+        // Метод для вычисления НОД трех натуральных чисел
+        static int GCD(int a, int b, int c)
+        {
+            return GCD(GCD(a, b), c);
+        }
+
+        static void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        static int Max(int a, int b)
+        {
+            return (a > b) ? a : b;
+        }
+
+        static void Main(string[] args)
+        {
+        Console.WriteLine("Упражнение 5.1");
+            
+        Console.WriteLine("Тестирование метода Max:");
+
+           
+        Console.WriteLine($"Max(5, 89) = {Max(5, 89)}"); 
+        Console.WriteLine($"Max(-11, -1) = {Max(-11, -1)}"); 
+        Console.WriteLine($"Max(0, 0) = {Max(0, 0)}"); 
+        Console.WriteLine($"Max(101, 500) = {Max(101, 500)}"); 
+        Console.WriteLine($"Max(5, 5) = {Max(5, 5)}");
+        Console.ReadLine();
+        
+        Console.WriteLine("Упражнение 5.2");
+        //метод который меняет местами значения двух передаваемых параметров
+
+        Console.WriteLine("Тестирование метода Swap:");
+
+        int x = 3;
+        int y = 7;
+
+        Console.WriteLine($"До обмена: x = {x}, y = {y}");
+
+        Swap(ref x, ref y);
+
+        Console.WriteLine($"После обмена: x = {x}, y = {y}"); 
+
+        int a = -1;
+        int b = 15;
+
+        Console.WriteLine($"До обмена: a = {a}, b = {b}");
+
+        Swap(ref a, ref b);
+
+        Console.WriteLine($"После обмена: a = {a}, b = {b}"); 
+        Console.ReadLine() ;
+
+        Console.WriteLine("Домашнее задание 5.1");
+            try
+            {
+                
+                int num1 = 26;
+                int num2 = 15;
+                Console.WriteLine($"НОД {num1} и {num2} равен {GCD(num1, num2)}");
+
+               
+                int num3 = 39;
+                Console.WriteLine($"НОД {num1}, {num2} и {num3} равен {GCD(num1, num2, num3)}");
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
